443710e9d601a747454a18ebfc75e22bc94a7539
Vineet Kumar <vineet.kumar@v2solutions.com>
2022-02-15 19:14:05 +0530

    added push api

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/broker/:sfid', function(req, res) {
 // });
 
 app.post('/adddata', function(req, res) {
-  client.query('INSERT INTO ' + salesforce.account + ' (name) VALUES ($1)', ['Robert'], function(error, data) {
+  client.query('INSERT INTO salesforce.account (name) VALUES ($1)', ['Robert'], function(error, data) {
     res.json(data);
   });
 });
