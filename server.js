552b829d068ac84a20add9d3686df3a6e69d4176
Vineet Kumar <vineet.kumar@v2solutions.com>
2022-02-17 13:52:48 +0530

    delete api change

--- server.js
+++ server.js
@@ -81,9 +81,13 @@ app.get( '/accountsdata', function(req, res) {
 // });
 
 app.delete('/deletedata/', function(req, res) {
+
+  console.log('Delete query_5 ' + req)
   console.log('Delete query_1 ' + req.username)
   console.log('Delete query_2 ' + req.body.username)
   console.log('Delete query_3 ' + req.params.username)
+  console.log('Delete query_4 ' + req.query.username)
+  
   client.query('DELETE FROM salesforce.account WHERE name = $1', [req.username], function(error, data) {
     res.json(data);
   });
