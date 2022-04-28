b121e749e9f28659bcfda375490e202b313cbc97
Vineet Kumar <vineet.kumar@v2solutions.com>
2022-04-28 11:50:30 +0530

    comments added

--- server.js
+++ server.js
@@ -14,76 +14,58 @@ var port = process.env.PORT || 5432;
 
 app.listen(port);
 
-console.log('Vineet :: Listening at: ' + port);
-
-var connectionString = process.env.DATABASE_URL || 'postgres://sqswklfxrepflp:faf953a77e258671c0c32c90a40900c207fe2489f42b0783e4e70483c1fbfcd2@ec2-18-215-8-186.compute-1.amazonaws.com:5432/df1vjjcla4od5f';
+var connectionString = process.env.DATABASE_URL || 'postgres://sqswklfxrepflc0c32c90a40900c207fe2489f42b0783e4e70483c1fcd2@ec2-18-215-8-142.compute-2.amazonaws.com:5432/df1vjjcla4od5f';
 
 if (process.env.DATABASE_URL !== undefined) {
-  console.log('VINEET DB reference ' + process.env.DATABASE_URL)
-  pg.defaults.ssl = true;
+    pg.defaults.ssl = true;
 }else{
   pg.defaults.ssl = true;
-  console.log('VINEET DB reference undefined')
-}
+ }
 
 //connect to db
 var client = new pg.Client(connectionString);
 
 //v2testpoc-2403
-client.host = 'ec2-18-215-8-186.compute-1.amazonaws.com';
+client.host = 'ec2-18-215-8-186.compute-3.amazonaws.com';
 client.database = 'abcd7777abcd';
 client.user = 'jjjjlfxrepflp';
-client.password = 'faf953a77e258671c0c32207fe2489f42b0783e4e70483c1fbfcd2';
+client.password = 'gaf953a77e258671c0c32207fe2489f42b0783e4e70483c1fbfcd2';
 
  client.connect();
 
 //table name
-var accountTable = 'salesforce.account';
-
-// app.post('/adddata', function(req, res) {
-//   client.query('INSERT INTO salesforce.account (name) VALUES ($1)', ['Vineet Kumar'], function(error, data) {
-//     res.json(data);
-//   });
-// });
+var emplTable = 'mydata.empl';
 
 //post request
 app.post('/adddata', function(req, res) {
-  client.query('INSERT INTO ' + accountTable + ' (name) VALUES ($1)', [req.body.username], function(error, data) {
+  client.query('INSERT INTO ' + var emplTable = 'mydata.empl';
+  + ' (name) VALUES ($1)', [req.user], function(error, data) {
     res.json(data);
   });
 });
 
 app.delete('/deletedata/', function(req, res) {
-  //console.log('Delete query_2 ' + req.body.username)
-
   //Query to delete a record  
-  client.query('DELETE FROM ' + accountTable + ' WHERE name = $1', [req.body.username], function(error, data) {
+  client.query('DELETE FROM ' + var emplTable = 'mydata.empl';
+  + ' WHERE name = $1', [req.user], function(error, data) {
     res.json(data);
   });
 });
 
-// app.delete('/deletedata/', function(req, res) {
-//   //console.log('Delete query_2 ' + req.body.username)
-
-//   //Query to delete a record  
-//   client.query('DELETE FROM ' + accountTable + ' WHERE name is null', function(error, data) {
-//     res.json(data);
-//   });
-// });
-
-
-app.put('/updatedata/', function(req, res) {
+app.put('/update/', function(req, res) {
     //  console.log('update query_2 Old name ==> ' + req.body.oldname) 
     //Query to update a record
-    client.query('UPDATE ' + accountTable + ' SET name = $1 WHERE name = $2', [req.body.newname, req.body.oldname ], function(error, data) {
+    client.query('UPDATE ' + var emplTable = 'mydata.empl';
+    + ' SET name = $1 WHERE name = $2', [req.body.newname, req.body.oldname ], function(error, data) {
     res.json(data);
   });
 });
 
 
 //Get request
-app.get( '/accountsdata', function(req, res) {
-  client.query('SELECT name FROM ' + accountTable ,  function(error, data) {
+app.get( '/empldata', function(req, res) {
+  client.query('SELECT data FROM ' + var emplTable = 'mydata.empl';
+  ,  function(error, data) {
     if(error != null){
       console.log(data)
       res.json(data);
@@ -92,20 +74,6 @@ app.get( '/accountsdata', function(req, res) {
   }
   });    
 });
-
-
-// app.get( '/accounts', function(req, res) {
-//   client.query('SELECT * FROM salesforce.account' ,  function(error, data) {
-//     if(error != null){
-//       console.log('Vineet :: Data retrieved from server' + data)
-//       res.json(data);
-//       //res.json({"Hello" : "World"})
-//   }else{
-//     console.log("Server :: Error " + error + " Data =>  " + data )
-//     res.json({"Vineet :: Data Value => " : data })
-//   }
-//   });    
-// });
 
 
 
